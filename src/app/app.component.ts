ffc9e2675629a1c6adbdf6f6e57a1222d29ffd62
jhansi-siripura <80917865+jhansi-siripura@users.noreply.github.com>
2023-12-26 16:10:53 +0530

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `<example-sidenav></example-sidenav>`
+})
+export class AppComponent {
+}
---
+++ src/app/example-sidenav.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'example-sidenav',
+  templateUrl: 'example-sidenav.component.html',
+  styleUrls: ['example-sidenav.component.css']
+})
+export class ExampleSidenavComponent {
+}
